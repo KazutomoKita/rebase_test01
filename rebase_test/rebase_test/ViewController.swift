65c2e058141d96996792d3dc06a9aad820697fc0
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-10 15:24:56 +0900

    commit_add03

--- rebase_test/rebase_test/ViewController.swift
+++ rebase_test/rebase_test/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
