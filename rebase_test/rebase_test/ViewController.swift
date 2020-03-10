05ea5fac29c09607f62fc52ee0ef7592b0e8a5f8
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-10 14:23:10 +0900

    ラベル＆ボタンセット

--- rebase_test/rebase_test/ViewController.swift
+++ rebase_test/rebase_test/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +20,6 @@ class ViewController: UIViewController {
     }
 
 
+    
 }
 
